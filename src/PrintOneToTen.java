a0a186e64db7895ebf843cb124f0d717f422c27d
joeactuary <goldman256@gmail.com>
2021-08-13 14:42:52 -0400

    Second Commit 1 to 100

--- src/PrintOneToTen.java
+++ src/PrintOneToTen.java
@@ -1,8 +1,6 @@
 public class PrintOneToTen {
     public static void main(String[] args) {
-        for  (int i =1; i<=10; i++) {
+        for (int i = 1; i <= 100; i++)
             System.out.println(i);
-
-        }
     }
 }
